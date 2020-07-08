a6b36cb08a86384cd91600f2e2631c338f41ad47
candescent-method-1839 <mqu000@citymail.cuny.edu>
2020-07-08 16:34:28 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,11 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(str)
+  return str.to_i - 1
+end
+
+def move(array, index, value="X")
+  array[index]= value
+  display_board(array)
+end
